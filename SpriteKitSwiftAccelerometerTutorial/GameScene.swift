702f9b73f78a21c95786ae11385de39c9072adb7
fransiska <fransiska.basoeki@gmail.com>
2017-06-07 22:13:37 +0700

    move position change to update function

--- SpriteKitSwiftAccelerometerTutorial/GameScene.swift
+++ SpriteKitSwiftAccelerometerTutorial/GameScene.swift
@@ -14,23 +14,29 @@ class GameScene: SKScene {
     // Create a sprite
     let player = SKSpriteNode(imageNamed: "Spaceship")
     
-    var posx:CGFloat = 0;
+    var posx:CGFloat = 0
+    var sign:CGFloat = 1
     
     func movePlayer() {
-        player.position = CGPoint(x: size.width/2+posx, y: size.height/2)
-        posx+=5.0
+        player.position = CGPoint(x: posx, y: size.height/2)
+        
+        
+        if(posx > size.width) {
+            sign = -1
+        } else if(posx < 0) {
+            sign = 1
+        }
+        posx += sign*5
     }
     
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
-        player.position = CGPoint(x: size.width/2, y: size.height/2)
+        player.position = CGPoint(x: posx, y: size.height/2)
         //to make the sprite appear, must add it as a child
         addChild(player)
-        
-        run(SKAction.repeatForever(
-            SKAction.sequence([
-                SKAction.run(movePlayer),
-                SKAction.wait(forDuration: 1.0)
-                ])))
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        movePlayer()
     }
 }
