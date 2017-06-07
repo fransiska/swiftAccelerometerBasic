5bf37c55934d1f18df03aa8a3c195a083d8fac04
fransiska <fransiska.basoeki@gmail.com>
2017-06-07 22:37:34 +0700

    get the accelerometer data

--- SpriteKitSwiftAccelerometerTutorial/GameScene.swift
+++ SpriteKitSwiftAccelerometerTutorial/GameScene.swift
@@ -9,17 +9,30 @@
 import SpriteKit
 import CoreMotion
 import GameplayKit
+import os.log
 
 class GameScene: SKScene {
     // Create a sprite
     let player = SKSpriteNode(imageNamed: "Spaceship")
+    let motionManager = CMMotionManager()
     
     var posx:CGFloat = 0
     var sign:CGFloat = 1
     
+    func degrees(radians:Double) -> Double {
+        return 180 / .pi * radians
+    }
+    
+    func handleDeviceMotionUpdate(deviceMotion:CMDeviceMotion) {
+        let attitude = deviceMotion.attitude
+        let roll = degrees(radians: attitude.roll)
+        let pitch = degrees(radians: attitude.pitch)
+        let yaw = degrees(radians: attitude.yaw)
+        print("r: \(roll), p: \(pitch), y: \(yaw)")
+    }
+    
     func movePlayer() {
         player.position = CGPoint(x: posx, y: size.height/2)
-        
         
         if(posx > size.width) {
             sign = -1
@@ -30,8 +43,24 @@ class GameScene: SKScene {
     }
     
     override func didMove(to view: SKView) {
+        os_log("didmove", type: .debug)
+        
         backgroundColor = SKColor.black
         player.position = CGPoint(x: posx, y: size.height/2)
+        
+        // get data of accelerometer async
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: {
+                (deviceMotion, error) -> Void in
+                if(error == nil) {
+                    self.handleDeviceMotionUpdate(deviceMotion: deviceMotion!)
+                } else {
+                    os_log("Error in starting device motion updates", type: .debug)
+                }
+            })
+        }
+
+        
         //to make the sprite appear, must add it as a child
         addChild(player)
     }
